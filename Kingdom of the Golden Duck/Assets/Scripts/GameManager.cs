6566fceb43d411ce56c25e08b8ae6400520f8e0d
McgrMik23 <mikaylamc2012@hotmail.com>
2023-03-01 14:47:46 -0800

    Sprite Changer + game Manager

---
+++ Kingdom of the Golden Duck/Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    // Booleans for what costumes the duck is wearing
+    // They are static so that they can be referenced from other scripts
+    public static bool regDuck = true;
+    public static bool uniDuck = false;
+    public static bool artDuck = false;
+    public static bool scubDuck = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Kingdom of the Golden Duck/Assets/Scripts/PlayerChangeSprite.cs
+++ Kingdom of the Golden Duck/Assets/Scripts/PlayerChangeSprite.cs
@@ -6,10 +6,34 @@ public class PlayerChangeSprite : MonoBehaviour
 {
     public SpriteRenderer spriteRenderer;
     public Sprite[] spriteArray; // Holds possible costumes for player
-    public Sprite newSprite; // The sprite that the player is changing to
+
+    
+    void Update() {
+        if (Input.GetKeyDown (KeyCode.Alpha1)) {
+            Debug.Log("1-unicorn");
+            GameManager.regDuck = false;
+            GameManager.uniDuck = true;
+            ChangeSprite(0);
+            Debug.Log("The Duck's costume is Unicorn :" + GameManager.uniDuck);
+        }
+        if (Input.GetKeyDown (KeyCode.Alpha2)) {
+            Debug.Log("2-artist");
+            ChangeSprite(1);
+            GameManager.regDuck = false;
+            GameManager.artDuck = true;
+        }
+        if (Input.GetKeyDown (KeyCode.Alpha3)) {
+            Debug.Log("3-scuba");
+            ChangeSprite(2);
+            GameManager.regDuck = false;
+            GameManager.scubDuck = true;
+        }
+
+
+    }
     
     //NEED TO ADD SYSTEM TO HAVE A SPECIFIC NUMBER SENT INTO THE FUNCTION
-    void ChangeSprite() {
-            spriteRenderer.sprite = spriteArray[0];
+    void ChangeSprite(int duckSprite) {
+            spriteRenderer.sprite = spriteArray[duckSprite];
     }
 }
