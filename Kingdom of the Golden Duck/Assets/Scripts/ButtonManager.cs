16b835c3ab96360712ba3b413110d05e12b0db0b
DoetriG <doetrighosh@gmail.com>
2023-03-23 13:41:13 -0700

    Test Scene

--- Kingdom of the Golden Duck/Assets/Scripts/ButtonManager.cs
+++ Kingdom of the Golden Duck/Assets/Scripts/ButtonManager.cs
@@ -8,14 +8,59 @@ using TMPro;
 
 public class ButtonManager : MonoBehaviour
 {
+    public CoinCounter coinCounterScript;
     public GameObject UnicornButton;
-    public void CharacterActivation()
+    public GameObject PaintDuckButton;
+    public GameObject ScubaDuckButton;
+
+    public void UnicornActivation()
     {
-        /*
-        if (CoinCounter.currentCoins - 5 != 0)
+        if (coinCounterScript.currentCoins - 5 >= 0)
         {
             UnicornButton.SetActive(false);
+            coinCounterScript.currentCoins -= 5;
+            coinCounterScript.coinText.text = "Coins: " + coinCounterScript.currentCoins.ToString();
+            StartCoroutine(WaitForUButton());
         }
-        */
+    }
+
+    public void PaintDuckActivation()
+    {
+        if (coinCounterScript.currentCoins - 5 >= 0)
+        {
+            PaintDuckButton.SetActive(false);
+            coinCounterScript.currentCoins -= 5;
+            coinCounterScript.coinText.text = "Coins: " + coinCounterScript.currentCoins.ToString();
+            StartCoroutine(WaitForPButton());
+        }
+    }
+
+    public void ScubaDuckActivation()
+    {
+        if (coinCounterScript.currentCoins - 5 >= 0)
+        {
+            ScubaDuckButton.SetActive(false);
+            coinCounterScript.currentCoins -= 5;
+            coinCounterScript.coinText.text = "Coins: " + coinCounterScript.currentCoins.ToString();
+            StartCoroutine(WaitForSButton());
+        }
+    }
+
+    IEnumerator WaitForUButton()
+    {
+        yield return new WaitForSeconds(15);
+        UnicornButton.SetActive(true);
+    }
+
+    IEnumerator WaitForPButton()
+    {
+        yield return new WaitForSeconds(15);
+        PaintDuckButton.SetActive(true);
+    }
+
+    IEnumerator WaitForSButton()
+    {
+        yield return new WaitForSeconds(15);
+        ScubaDuckButton.SetActive(true);
     }
 }
